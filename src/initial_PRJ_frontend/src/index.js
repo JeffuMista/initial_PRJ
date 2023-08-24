deda68fd244974118ee4e4847f08c1eb02526b91
Jeff Devops <gnmista@gmail.com>
2023-08-24 15:27:09 +0300

    ICP TEST PROJECT

---
+++ src/initial_PRJ_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { initial_PRJ_backend } from "../../declarations/initial_PRJ_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await initial_PRJ_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
